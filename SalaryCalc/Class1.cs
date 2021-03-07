8de8eb1ebf0caef9ad8d1a5c9ef1aac07ea54571
ISC191837 <isc191837@itsatlixco.edu.mx>
2021-03-07 15:38:51 -0600

    Add project files.

---
+++ SalaryCalc/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalc
+{
+    public class Calculator
+    {
+        public float getSalaryByHours(int hoursWorked, float paidByHours)
+        {
+            float result = 0;
+
+            if (hoursWorked > 30)
+            {
+                result = paidByHours * 30;
+                result = result + ((paidByHours * 2) * (hoursWorked - 30));
+            } 
+            else
+            {
+                result = hoursWorked * paidByHours;
+            }
+
+            return result;
+        }
+
+        public float getTax(float salary)
+        {
+            float result;
+            if (hasTax(salary))
+            {
+                result = (float)(salary * 0.15);
+            }
+            else
+            {
+                result = (float)(salary * 0.10);
+            }
+
+            return result;
+        }
+        
+        public bool hasTax(float salary)
+        {
+            if (salary > 3000)
+            {
+                return true;
+            }
+
+            return true;
+        }
+    }
+}
